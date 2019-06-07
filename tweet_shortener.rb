5bb683738caf9290afae4beac8decf4fb6bdc27c
The Natural <philip.smalls86@gmail.com>
2019-06-07 00:14:18 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -16,12 +16,6 @@ def dictionary
 end
 
 def word_substituter(tweet)
-  tweet.split(", ").each do |k,v|
-   dictionary.keys.each do |word|
-      if word == k 
-        tweet[k] == tweet[word]
-      end 
-    end.join(" ")
-  end
-
+  tweet.split(", ")
+end
 
