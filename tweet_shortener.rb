f992eb76027695b9973a82d7ff9072fc0f87eb9c
The Natural <philip.smalls86@gmail.com>
2019-06-07 00:20:32 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,27 +1,17 @@
 # Write your code here.
 
-def dictionary 
-  words = {
-    :hello => "hi",
-    :to => "2",
-    :two => "2",
-    :too => "2",
-    :for => "4",
-    :four => "4",
-    :be => "b",
-    :you => "u",
-    :at => "@",
-    :and => "&"
-}
-end
 
 def word_substituter(tweet)
-  tweet.split(",").each do |word|
-    dictionary.each do |key, value|
-      if word == key
+  dict = dictionary
+  words = tweet.split(" ")
+
+  words.collect do |word|
+    dictionary.collect do |key, value|
+      if word == key 
         word.replace(value)
-      end
+      end 
     end
-  end.join("")
-end
+  end 
+  words.join(" ")
+end 
 
